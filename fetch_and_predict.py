d96d512f4e75ab8f10dfae207a83f76c061ad7ae
johnobodai <johnodaiobodai@gmail.com>
2024-07-10 07:28:12 +0000

    Add function to fetch the latest water quality entry

--- fetch_and_predict.py
+++ fetch_and_predict.py
@@ -6,3 +6,12 @@ from tensorflow import keras
 # API endpoint
 API_URL = "http://localhost:5000/waterquality"
 
+def fetch_latest_entry():
+    """Fetch the latest water quality entry from the API"""
+    response = requests.get(API_URL)
+    if response.status_code == 200:
+        entries = response.json()
+        if entries:
+            return entries[-1]  # Return the last (latest) entry
+    return None
+
